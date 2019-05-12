f0f29cdd8b6a40350dd754f2e0a7b37479c5a5de
Junseok Oh <kotohana5706@edcan.kr>
2019-05-12 23:28:09 +0900

    [PhotoEditActivity] Add Photo Make Component

--- app/src/main/java/com/buddman/zepespot/PhotoEditActivity.kt
+++ app/src/main/java/com/buddman/zepespot/PhotoEditActivity.kt
@@ -1,24 +1,45 @@
 package com.buddman.zepespot
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.provider.MediaStore
 import com.buddman.zepespot.utils.NetworkDrawable
 import com.xiaopo.flying.sticker.DrawableSticker
 import kotlinx.android.synthetic.main.activity_photo_edit.*
 
-
 class PhotoEditActivity: BaseActivity() {
     override fun setDefault() {
+        save.setOnClickListener {
+        }
+        sendTakePhotoIntent()
+    }
 
+    private fun sendTakePhotoIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, 1234)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1234 && resultCode == Activity.RESULT_OK) {
+            val extras = data!!.extras
+            val imageBitmap = extras!!.get("data") as Bitmap
+            editView.addSticker(DrawableSticker(BitmapDrawable(resources, imageBitmap)))
+            addMe()
+        }
+    }
+
+    private fun addMe(){
         editView.apply {
             addSticker(DrawableSticker(NetworkDrawable.drawableFromUrl(
                     "http://47.74.149.35/api/photo/1zUcXMFLQ5FIGkaMBtc28K/?hashCodes=1XBRWB,7V17S2&width=512"
             )))
         }
-
-        save.setOnClickListener {
-        }
     }
-
-
     override val viewId: Int = R.layout.activity_photo_edit
     override val toolbarId: Int = 0
 }
